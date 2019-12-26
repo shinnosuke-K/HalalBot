2ba5766df80d79676377b5b45a3022fa055c956f
shinnosuke-K <shin.050195@gmail.com>
2019-12-26 16:32:56 +0900

    test

--- ocr/aws.go
+++ ocr/aws.go
@@ -33,7 +33,7 @@ func DoOCR(imageContent io.ReadCloser) {
 	resp, err := svc.DetectText(params)
 	errorHand.HandleError(err)
 	for _, text := range resp.TextDetections {
-		log.Print(text.DetectedText)
+		log.Print(*text.DetectedText)
 	}
 
 }
